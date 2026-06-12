de8ac12405f87828111ef1d70acccadbea8d78e1
primary868 <primary686@gmail.com>
2026-06-12 15:40:38 -0400

    add

--- falcon.py
+++ falcon.py
@@ -1,51 +1,108 @@
-import subprocess, argparse, glob, os, sys, json
+import os, sys, glob, subprocess, argparse, shutil, json
+from PIL import Image #type: ignore
 
-# constants
-COMMANDS = {
-    'alac':["-c:a", "alac", "-c:v", "copy"],
-    'mp3':['-ab', '320k', '-c:v', 'copy']
-}
-EXTENSIONS = {
-    'alac':'m4a',
-    'mp3':'mp3'
-}
+######### Art Conversion Functions #########
 
-# note: this script is not linux-safe.
-if os.name != 'nt': print("This script is only compatible with Windows environments."); sys.exit(2)
+def cover_playdate(img: Image): 
+  outfile = os.path.join(OUTFOLDER,'_artwork.gif')
+  img.resize((128, 128), Image.Resampling.LANCZOS).convert("1").save(outfile,'GIF')
+  print("Playdate-format cover art saved.")
 
-# define arguments from commandline and json
+def cover_ipod(img: Image): 
+  outfile = os.path.join(OUTFOLDER,'cover.jpg')
+  img.resize((600,600)).save(outfile)
+  print("iPod/iTunes format cover saved.")
+
+######### Functions #########
+
+def run_command(cmd: list) -> int: return subprocess.run(cmd, check=True).returncode
+
+def make_cover_image() -> Image:
+  print("Getting source cover art")
+  
+  # look for cover.jpg
+  if os.path.exists(cov := os.path.join(ARGS.infolder,"cover.jpg")): return Image.open(cov)
+  else: print("Could not find cover.jpg -- extracting from FLACs")
+
+  # extract from one of the flac files
+  extracted_tmp = ("extracted_tmp.jpg")
+  source_file = INFILES[0]
+  run_command(['ffmpeg', '-loglevel', 'error', '-hide_banner', '-i', source_file, '-an', '-vcodec', 'copy', extracted_tmp])
+  output = Image.open(extracted_tmp)
+  os.remove(extracted_tmp)
+  print(f"Got cover art from {source_file}")
+  return output
+
+def album_artist_map() -> list:
+  print("--- Getting album artist ---")
+  try: 
+    album_artist = subprocess.run(
+      ['ffprobe', '-v', 'error', '-show_entries', 'format_tags=album_artist', '-of', 'default=noprint_wrappers=1:nokey=1', INFILES[0]],
+      check=True,
+      capture_output=True
+    ).stdout.decode('utf-8').strip()
+    if album_artist == '': raise
+  except: 
+    print("Could not find album artist in source files. Outfiles will use original artist tags.")
+    return []
+  else: 
+    print(f"Album artist is: '{album_artist}'")
+    return['-metadata',f'artist={album_artist}']
+
+######### Prereq #########
+
 try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('format', help='The format of the output files.', choices=['mp3','alac'])
-    with open("falcon-config.json","r") as file:
-        js = json.load(file)
-        ARGS = {
-            "format":parser.parse_args().format,
-            "in_list":js['in_list'],
-            "out_folder":js['out_folder'],
-            "ffmpeg_path":js['ffmpeg_path']
-        }
-    
-except Exception as e: print(f"Error parsing arguments: {e}"); sys.exit(2)
+  for dep in ['ffmpeg','ffprobe']: assert shutil.which(dep), f"Dependency not found: {dep}"
+  
+  with open('./falcon_config.json','r') as file:
+    data = json.load(file)
+    FLAGS = data['out_types']
+    OUTFOLDER = data['outfolder']
+  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('infolder',help="folder of flac files to convert")
+  parser.add_argument('out_type',choices=list(FLAGS.keys()),help="Type of conversion to perform")
+  parser.add_argument('--overwrite', '-y',action='store_true',help='Overwrite existing output files')
+  ARGS = parser.parse_args()
+  ARGS.infolder = os.path.expanduser(ARGS.infolder)
 
-# parse infile
-with open(ARGS['in_list']) as infile: album_folders = [line.rstrip().replace('\"','') for line in infile]
+  INFILES = glob.glob(os.path.join(ARGS.infolder,'*.flac'))
+  assert INFILES, "No flac files found in input folder."
+
+  OUTFOLDER = os.path.expanduser(OUTFOLDER).replace(r'%A%',os.path.basename(ARGS.infolder)).replace(r'%F%',ARGS.out_type)
+  if not os.path.exists(OUTFOLDER): os.makedirs(OUTFOLDER)
+  print(f"Saving to folder: {OUTFOLDER}")
+
+  THIS = FLAGS[ARGS.out_type]
+
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+  sys.exit(2)
+
+######### Main #########
 
 try:
-    for album in album_folders:
-        # create album output folder
-        dest_folder = f"{ARGS['out_folder']}\\{os.path.basename(os.path.dirname(album))} - {os.path.basename(album)}"
-        os.makedirs(dest_folder)
-        print(f"Creating: {dest_folder}")
+  if (func_name := THIS['cover_func']): 
+    print("=== Make output cover art ===")
+    source_img = make_cover_image()
+    globals().get(func_name)(source_img)
+  
+  print("=== Make base ffmpeg command ===")
+  cmd = ['ffmpeg','-loglevel','error','-hide_banner']
+  if ARGS.overwrite: cmd += ['-y']
+  cmd += ['-i',9] # 9 is replaced by flac file path during use
+  cmd += THIS['output_format'].split()
+  if THIS['use_album_artist']: cmd += album_artist_map()
+  print("Command made.")
 
-        # run ffmpeg
-        for file in glob.glob(f"{album}\\*.flac"):
-            print(f"  Processing {os.path.basename(file)}...")
-            p = subprocess.run(
-                [ARGS['ffmpeg_path'], "-i", file] + 
-                COMMANDS[ARGS['format']] +
-                [f"{dest_folder}\\{os.path.basename(file)[:-5]}.{EXTENSIONS[ARGS['format']]}", "-hide_banner", "-loglevel", "error"])
+  print("=== Convert FLAC files ===")
+  for flac in INFILES:
+    outfile = os.path.join(OUTFOLDER, os.path.splitext(os.path.basename(flac))[0]+'.'+THIS['ext'])
+    this_cmd = [flac if arg == 9 else arg for arg in cmd]+[outfile] # fill in infile and outfile paths
+    run_command(this_cmd)
+    print(f"Done: {os.path.basename(outfile)}")
 
-except Exception as e: print(f"Error during conversion: {e}"); sys.exit(1)
-
-sys.exit(0)
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+  sys.exit(1)
+else: print("=== All operations complete ===")
--- readme.md
+++ readme.md
@@ -1,12 +1,30 @@
-"flac converter" -> "flaccon" -> "falcon"
-Makes copies of FLAC files in specified file type (ALAC or 320k mp3).
+# falcon FLAC Converter
+Written as an easy interface for ffmpeg, for converting flac into a few formats I commonly use.
+("flac converter" -> "flaccon" -> "falcon")
 
 # Usage
-`python ./falcon.py [mp3/alac]`
+```
+falcon.py [-h] [--overwrite] infolder out_type
 
-Settings are stored in `./falcon-config.json`:
-- `ffmpeg_path`: path to ffmpeg executable
-- `in_list`: text file where each line is a path to an album of .flac files to work with.
-- `out_folder`: where the files produced by the script are stored.
+positional arguments:
+  infolder          folder of flac files to convert
+  out_type          Type of conversion to perform; valid values come from falcon_config.json
 
-An example `falcon-config.json` is provided. It must be stored in the same folder as the main `falcon.py` script.
+options:
+  -h, --help        show this help message and exit
+  --overwrite, -y   Overwrite existing output files
+```
+
+# Defining Conversion Types
+Conversion types (arguments for ffmpeg) are defined outside the script. In `./falcon_config.json`, add an entry to the `out_types` dict in this format:
+```json
+"play":{
+  "ext":"mp3",
+  "output_format":"-c:a libmp3lame -vn -b:a 128k",
+  "cover_func":"cover_playdate", // cover_playdate() exists in falcon to make a discrete album art image in the output.
+  "use_album_artist":true // Use source files' 'album artist' tag as infiles' 'artist' tag
+}
+```
+To convert to this format, the command would be: `falcon.py /path/to/input/folder play`
+
+To create a custom cover art image file, add a function to the script which takes `PIL.Image` data as input and saves an image. Two example functions are at the top of the source code: `cover_playdate` and `cover_ipod`.
